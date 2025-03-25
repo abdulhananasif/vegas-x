bd6e8aaf2b67b765f95e5f1a6d852f7ab7311ab5
Junaid Saroya <junaidsaroya1611@gmail.com>
2025-03-25 12:50:58 +0500

    LoginForm

--- src/components/forms/Login/index.tsx
+++ src/components/forms/Login/index.tsx
@@ -1,11 +1,36 @@
-import Button from '../../ui/Button';
-import Input from '../../ui/Input';
-
 const LoginForm = () => {
+  const handleLogin = async () => {};
   return (
-    <form>
-      <Input className="w-full" name="username" type="text" />
-      <Button name="Login" />
+    <form onSubmit={handleLogin}>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="USERNAME..."
+          className="w-full px-2 py-1 bg-white text-sm border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          required
+        />
+      </div>
+
+      <div className="relative">
+        <input
+          placeholder="PASSWORD..."
+          className="w-full px-2 py-1 bg-white text-sm border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          required
+        />
+      </div>
+
+      <div className="mt-1 mb-4 flex flex-col justify-center text-center">
+        <span className="text-white text-xs mt-2">REMEMBER ME</span>
+        <button
+          type="submit"
+          className="mx-auto w-20 py-1 px-4 !bg-blue-500 my-3 cursor-pointer text-white rounded-md"
+        >
+          ENTER
+        </button>
+        <p className="text-white text-sm">sales[at]vegas-x.net</p>
+
+        <p className="text-white text-sm mt-5">Forgot password?</p>
+      </div>
     </form>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
+import LoginForm from "../components/forms/Login";
+
 const Login = () => {
-  const handleLogin = async () => {
-    
-  };
+  
   return (
     <div
       className="flex flex-col min-h-screen"
@@ -25,37 +25,7 @@ const Login = () => {
         <div className="w-full lg:w-1/2">
           <div className=" flex justify-center">
             <div className="max-w-md w-[20rem] py-6 px-8 rounded-2xl bg-gray-400 opacity-30%">
-              <form onSubmit={handleLogin}>
-                <div className="mb-4">
-                  <input
-                    type="text"
-                    placeholder="USERNAME..."
-                    className="w-full px-2 py-1 bg-white text-sm border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    required
-                  />
-                </div>
-
-                <div className="relative">
-                  <input
-                    placeholder="PASSWORD..."
-                    className="w-full px-2 py-1 bg-white text-sm border border-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    required
-                  />
-                </div>
-
-                <div className="mt-1 mb-4 flex flex-col justify-center text-center">
-                  <span className="text-white text-xs mt-2">REMEMBER ME</span>
-                  <button
-                    type="submit"
-                    className="mx-auto w-20 py-1 px-4 !bg-blue-500 my-3 cursor-pointer text-white rounded-md"
-                  >
-                    ENTER
-                  </button>
-                  <p className="text-white text-sm">sales[at]vegas-x.net</p>
-
-                  <p className="text-white text-sm mt-5">Forgot password?</p>
-                </div>
-              </form>
+              <LoginForm />
             </div>
           </div>
         </div>
