2e9bffab1447494739dc1dc8c4737f8d9b954b2a
Abdul Hanan Asif <abdulhananasif5643@gmail.com>
2025-03-26 13:04:25 +0500

    footer fixed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,7 +9,8 @@ const Footer = () => {
     <div className="relative w-full h-[7.79rem]">
       <Transaction />
       <Profile />
-      <Image src="/navbar.png" alt="navbar" />
+      <Image src="/navbar.png" alt="navbar" className="hidden lg:block" />
+      <div className="h-full bg-[#571BA0] w-screen" />
       <Information />
       <Helper />
     </div>
